d2ed012e373586a690fa31864e2a86a51d041b12
Cait Gorevin <caitlin.gorevin@gmail.com>
2017-07-03 22:30:24 +0000

    Create StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def collect
+  end
+
+  def donate
+  end
+
+  def support
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/collect'
+
+  get 'static_pages/donate'
+
+  get 'static_pages/support'
+
   resources :hotspots, only: [:new, :create]
 end
