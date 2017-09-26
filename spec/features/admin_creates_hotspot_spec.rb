3fdbfd05ae1d2a164af3a1551ea7a7c8d6d92374
Crystal Ramirez <crystal9563@gmail.com>
2017-09-26 21:30:53 +0000

    updated changes

--- spec/features/admin_creates_hotspot_spec.rb
+++ spec/features/admin_creates_hotspot_spec.rb
@@ -27,13 +27,5 @@ feature "admin creatures hotspot" do
         expect(page).to have_content "Hotspot could not be saved."
     end
     
-    def login_admin
-        admin = create(:admin, password: "foobar")
-        visit "/"
-        click_link("Login")
-        fill_in("Email", with: admin.email)
-        fill_in("Password", with: "foobar")
-        
-        click_button("Log in")
-    end
+    
 end
