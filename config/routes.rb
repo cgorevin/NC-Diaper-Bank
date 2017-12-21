ddf6b3f6d4c1adb51b98bee179509847626131ef
Crystal Ramirez <crystal9563@gmail.com>
2017-12-21 00:58:55 +0000

    added changes fixed controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,7 @@ Rails.application.routes.draw do
   devise_for :admins, :skip => [:registrations]
  
   get 'static_pages/home'
-<<<<<<< HEAD
-  
-  post 'static_pages/home'
-  
-  get 'static_pages/about'
 
-  get 'static_pages/collect'
-
-  get 'static_pages/donate'
-
-  get 'static_pages/support'
-=======
->>>>>>> 6e7ad111ef6ab7936c2c2658acf7c9cbb268a5ee
 
   root 'static_pages#home'
   
