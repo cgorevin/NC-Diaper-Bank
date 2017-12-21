cec9ee9e4c1416a8282412913c9ff9fbbab3c005
Cait Gorevin <caitlin.gorevin@gmail.com>
2017-12-21 00:16:32 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,7 @@ Rails.application.routes.draw do
   devise_for :admins, :skip => [:registrations]
  
   get 'static_pages/home'
-  
-  post 'static_pages/home'
-  
-  get 'static_pages/about'
 
-  get 'static_pages/collect'
-
-  get 'static_pages/donate'
-
-  get 'static_pages/support'
 
   root 'static_pages#home'
   
