770891eec4f6ff82c45a5a913b88b6eb2b3158e2
Cait Gorevin <caitlin.gorevin@gmail.com>
2018-08-21 12:06:46 -0400

    Troubleshooting for Heroku app error in Hotspot model

--- app/models/hotspot.rb
+++ app/models/hotspot.rb
@@ -44,6 +44,7 @@ class Hotspot < ActiveRecord::Base
     def self.address_sort(current_location)
         self.near([current_location.latitude, current_location.longitude])
              .limit(3)
+             .to_a
              .sort_by do |hotspot|
                hotspot.get_distance(current_location)
              end
